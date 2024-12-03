e7c519ffec1005e2f89c7a3b85ab3be3fba3fb32
ivencomur <ivancortes@hotmail.com>
2024-12-03 17:12:19 -0500

    CORRECTING FOR "ALMOST THERE"

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
 let pokemonList= [
-    {name:"Articuno", height: " Height: 1.7 m;", types: [" Types: ice"," flying"]}, 
-    {name:" Charizard ", height: " Height: 1.7 m;", types: [" Types: fire"," flying"]}, 
-    {name:" Butterfree", height: " Height: 1.1 m;", types: [" Types: bug"," flying"]}, 
-    {name:" Pidgeot", height: " Height: 1.5 m;", types: [" Types:  lying"," normal"]}
+    {name:"Articuno", height: 1.7, types: ["ice","flying."], imgFile: "img/articuno.svg"}, 
+    {name:"Charizard", height: 1.7, types: ["fire"," flying."], imgFile: "img/charizard.svg"}, 
+    {name:"Butterfree", height: 1.1, types: ["bug"," flying."], imgFile: "img/butterfree.svg"}, 
+    {name:"Pidgeot", height: 1.5, types: ["flying"," normal."], imgFile: "img/pidgeot.svg"}
 ];
 // I included a condition for Pokemon´s height
- 
+let maxheight = 1.69
 // This is the start of the loop, I assume.
 for (let i = 0; i < pokemonList.length; i++) {
-    //Asking if the height is bigger than 1.69(because the tallest Pokémon is 1.7; and if it is comfirmed as true. Also, if Any pokemonhas been labeled yet.
-    if (pokemonList[i].height > " Height: 1.69 m;") {
-    //The original output I wrote plus the new output label.
-    document.write (pokemonList [i].name  + " ("+ pokemonList [i].height + pokemonList [i].types+") >>>Wow, that Pokémon is tall!<<<.");
-    //This part was really confusing for me. I used AI suggestions because my code was disconected from the last statement/output.      
-   }   
+    //setting up conditionals
+    if (pokemonList[i].height > maxheight) {       
+        //valid concatenation
+        let concatText = "<p>" + pokemonList [i].name  + " ("+pokemonList [i].height + pokemonList [i].types+") Wow, that Pokémon is tall!." + "</p>"
+        let interpolatedText = `<p>${pokemonList[i].name}. Height: ${pokemonList [i].height}m. Types: ${pokemonList[i].types} Wow, that Pokémon is tall!.</p>`
+        let pokemonImage = `<img src="${pokemonList[i].imgFile}"/>`        
+        document.write (interpolatedText);
+        document.write(pokemonImage)
+        }   
     else {
-     //The original output I wrote needed also to be included in the code. Otherwise my HTML, I guess, wouldn´t have shown anything.
-    document.write(pokemonList[i].name + " (" + pokemonList[i].height + pokemonList[i].types + ").");
+        let concatText = "<p>" + pokemonList[i].name + " (" + pokemonList[i].height + pokemonList[i].types + ")."+"</p>"
+        let interpolatedText = `<p>${pokemonList[i].name}. Height: ${pokemonList[i].height}m. Types: ${pokemonList[i].types}</p>`
+        let pokemonImage = `<img src="${pokemonList[i].imgFile}"  width: '200' height: '200'/>`       
+        document.write(interpolatedText);
+        document.write(pokemonImage)       
    }
+   
 }
-
